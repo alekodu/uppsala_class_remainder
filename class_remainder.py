0b87ee5572ce41625e0aaa8a2b36b7a7bca058cc
alekodu <alekodu@gmail.com>
2017-11-18 21:05:20 +0100

    12

--- class_remainder.py
+++ class_remainder.py
@@ -15,7 +15,6 @@ def location(event):
 def get_latest_session(sessions):
     right_now = datetime.datetime.now()
     smallest_diff = sessions[0][1] - right_now
-    print('smallest diff %d', smallest_diff)
     s = sessions[0]
     for session in sessions:
         current_diff = session[1] - right_now
@@ -32,7 +31,8 @@ def get_stuff(url):
     sessions = [(lecturize(e), start_time(e), location(e)) for e in gcal.walk('vevent')]
 
     session = get_latest_session(sessions)
-    return str(session[0]) + '\n' + str(session[1]) + '\n' + str(session[2])
+    return session
+    #return str(session[0]) + '\n' + str(session[1]) + '\n' + str(session[2])
     #return str(sessions[0][0]) + '\n' + str(sessions[0][1]) + '\n' + str(sessions[0][2])
 
 #print get_stuff('https://se.timeedit.net/web/uu/db1/schema/s.ics?i=yQ99053X5Z69Q096546X6Z690544400259053Q9561Y59Y504YX5953556Z0XW90nQY255')
